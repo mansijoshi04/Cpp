d27b810f137594c23ec56b581a624daacc7752d3
Mansi Joshi <josh0080@algonquinlive.com>
2024-01-21 22:55:27 -0500

    Finished Constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,26 @@
-
 #include<iostream>
+using namespace std;
 
-int main(int argc, char **argv)
-{
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+namespace CST8219 {
+    class Vehicle 
+    {
+        private:
+        int numWheels;
+        int numDoors;
+
+        public:
+        Vehicle(int w, int d) : numWheels(w), numDoors(d) {}
+        Vehicle(int w) : Vehicle(w, 4) {}
+        Vehicle() : Vehicle(4) {}
+    };
 }
+
+    int main(int argc, char** argv)
+    {
+
+        CST8219::Vehicle vehicle1(4, 2); 
+        CST8219::Vehicle vehicle2(2);    
+        CST8219::Vehicle vehicle3;       
+
+     return 0;
+    }
